7af90a591e5626aa398a4b9a4d4750c734196b56
chooooh <oh3@kth.se>
2022-01-18 14:21:22 +0100

    first commit

---
+++ src/main/java/se/kth/recruitmentapp/RecruitmentAppApplication.java
@@ -0,0 +1,13 @@
+package se.kth.recruitmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentAppApplication.class, args);
+	}
+
+}
