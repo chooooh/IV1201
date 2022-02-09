d3ec1fd7fdd3067cdd787efe85c983d698b798dd
malmisen <sebastien.malmberg@hotmail.com>
2022-02-09 16:25:10 +0100

    More changes to navbar

--- src/main/java/se/kth/recruitmentapp/presentation/controller/NavController.java
+++ src/main/java/se/kth/recruitmentapp/presentation/controller/NavController.java
@@ -12,17 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Scope("session")
 public class NavController {
-    static final String ROOT_PAGE_URL       = "/";
     static final String WELCOME_PAGE_URL    = "welcome";
     static final String LOGIN_PAGE_URL      = "login-user";
     static final String SIGNUP_PAGE_URL     = "sign-up";
-
-    /**
-     * If no page is specified, redirect to the welcome page.
-     * @return A response that redirects the browser to the welcome page.
-     */
-    @GetMapping(ROOT_PAGE_URL)
-    public String showRootPage(){ return "redirect:" + WELCOME_PAGE_URL;}
 
     /**
      * A get request for the welcome page.
