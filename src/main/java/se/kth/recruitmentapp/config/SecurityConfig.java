ee7a78739ce9c7459138917f1c14b9ed8d46c28a
malmisen <sebastien.malmberg@hotmail.com>
2022-02-09 10:45:21 +0100

    adsadsad

--- src/main/java/se/kth/recruitmentapp/config/SecurityConfig.java
+++ src/main/java/se/kth/recruitmentapp/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,7 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(encoder());
     }
-
 
     /**
      * Configure Authorization to URLs and Login/logout logic.
