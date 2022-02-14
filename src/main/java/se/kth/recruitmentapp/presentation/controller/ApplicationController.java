45857bc3e070a798bb96631afc36b3a009546148
malmisen <sebastien.malmberg@hotmail.com>
2022-02-14 19:38:56 +0100

    Logger

--- src/main/java/se/kth/recruitmentapp/presentation/controller/ApplicationController.java
+++ src/main/java/se/kth/recruitmentapp/presentation/controller/ApplicationController.java
@@ -1,33 +1,40 @@
 package se.kth.recruitmentapp.presentation.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
+import se.kth.recruitmentapp.domain.Competence;
 import se.kth.recruitmentapp.domain.Person;
 import se.kth.recruitmentapp.presentation.forms.CompetenceForm;
 import se.kth.recruitmentapp.presentation.forms.CreateAccountForm;
 import se.kth.recruitmentapp.presentation.forms.LoginForm;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class ApplicationController {
     static final String ADD_COMPETENCE_POST_URL = "add-competence";
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationController.class);
 
     @PostMapping("/" + ADD_COMPETENCE_POST_URL)
     public String processCompetence(@Valid CompetenceForm competenceForm, BindingResult bindingResult, Model model) {
 
+
+        LOGGER.info("Competence form" + "\n"
+                + "SELECTED competence: " + competenceForm.getSelectedCompetence() +"\n"
+                + "SELECTED years: "+ competenceForm.getYearsOfExperience());
         if (bindingResult.hasErrors()) {
             System.out.println("competence bindings has errors");
         }
 
-        System.out.println("Selected comp:" + competenceForm.getSelectedCompetence());
-        System.out.println("Years: " + competenceForm.getYearsOfExperience());
-
         competenceForm.addCompetence(competenceForm.getSelectedCompetence(), String.valueOf(competenceForm.getYearsOfExperience()));
-
+        List<Competence> competenceList =
+        model.addAttribute("competences", competenseList);
         model.addAttribute("competenceForm", new CompetenceForm(competenceForm.getCompetences(), competenceForm.getCompetenceList()));
         return NavController.APPLY_PAGE_URL;
     }
--- src/main/java/se/kth/recruitmentapp/presentation/controller/NavController.java
+++ src/main/java/se/kth/recruitmentapp/presentation/controller/NavController.java
@@ -65,8 +65,8 @@ public class NavController {
     @GetMapping("/" + APPLY_PAGE_URL)
     public String showApplyPageView(Model model){
         List<Competence> competenceList = competenceService.getAllCompetences();
-        //model.addAttribute("competences", competenceList);
-        model.addAttribute("competenceForm", new CompetenceForm(competenceList));
+        model.addAttribute("competences", competenceList);
+        model.addAttribute("competenceForm", new CompetenceForm());
         return APPLY_PAGE_URL;
     }
 
