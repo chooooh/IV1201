737c0045351f9fc78227a275af5129fa34e4131b
Francesco Giangiulio <giangiulio.ingemar@gmail.com>
2022-02-22 15:55:39 +0100

    yoe validation functioning

--- src/main/java/se/kth/recruitmentapp/presentation/forms/CreateAccountForm.java
+++ src/main/java/se/kth/recruitmentapp/presentation/forms/CreateAccountForm.java
@@ -13,31 +13,24 @@ import javax.validation.constraints.*;
 @Data
 public class CreateAccountForm {
     @NotNull
-    @NotBlank(message = "{create-acct.applicant-firstname.missing}")
     @Size(min = 2, max = 30, message = "{create-acct.applicant-firstname.length}")
     private String firstname;
     @NotNull
-    @NotBlank(message = "{create-acct.applicant-surname.missing}")
     @Size(min = 2, max = 30, message = "{create-acct.applicant-surname.length}")
     private String surname;
     @NotNull
-    @NotBlank(message = "{create-acct.applicant-pnr.missing}")
     @Pattern(regexp = "^\\d{6}(?:\\d{2})?[-\\s]?\\d{4}$", message = "{create-acct.applicant-pnr.format}")
     private String pnr;
     @NotNull
-    @NotBlank(message = "{create-acct.applicant-email.missing}")
     @Email(message = "{create-acct.applicant-email.format}")
     private String email;
     @NotNull
-    @NotBlank(message = "{create-acct.applicant-username.missing}")
     @Size(min = 5, max = 30, message = "{create-acct.applicant-username.length}")
     private String username;
     @NotNull
-    @NotBlank(message = "{create-acct.applicant-password.missing}")
     @Size(min = 6, max = 18, message = "{create-acct.applicant-password.length}")
     private String password;
     @NotNull
-    @NotBlank(message = "{create-acct.applicant-password.missing}")
     @Size(min = 6, max = 18, message = "{create-acct.applicant-password.length}")
     private String confirmPassword;
 
