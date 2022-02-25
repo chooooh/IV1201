05bb0840d1eef01ba7c8f0fbaa227dcd8b3a6066
chooooh <oh3@kth.se>
2022-02-25 19:00:51 +0100

    preparing for branch

---
+++ src/test/java/se/kth/recruitmentapp/presentation/controller/RecruitmentControllerTest.java
@@ -0,0 +1,10 @@
+package se.kth.recruitmentapp.presentation.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(RecruitmentController.class)
+class RecruitmentControllerTest {
+
+}
