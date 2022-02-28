1854f0fea48375a1c790db4a1a5b87dbf1db84e7
chooooh <oh3@kth.se>
2022-02-28 16:26:54 +0100

    tests are passing. Questionable if correct

--- src/test/java/se/kth/recruitmentapp/presentation/controller/RecruitmentControllerTest.java
+++ src/test/java/se/kth/recruitmentapp/presentation/controller/RecruitmentControllerTest.java
@@ -1,24 +1,14 @@
 package se.kth.recruitmentapp.presentation.controller;
 
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.config.ConfigFileApplicationListener;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -27,6 +17,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class RecruitmentControllerTest {
     // private RecruitmentController controller;
+    private static final String REDIRECT = "redirect:/";
+    private static final String LOCALHOST = "http://localhost/";
     @Autowired
     private MockMvc mockMvc; // does not start server
 
@@ -42,21 +34,36 @@ class RecruitmentControllerTest {
     }
 
     @Test
-    @WithMockUser(authorities = {"applicant"})
-    public void testRecruitmentPageApplicantNonAuthorized() throws Exception {
-        mockMvc.perform(get("/" + RecruitmentController.RECRUITMENT_URL))
-                .andExpect(status().is4xxClientError())
-                .andExpect(content().string(
-                        containsString("Login")
-                ));
+    @WithMockUser(authorities = {"recruiter"})
+    public void testRecruitmentPageRedirectNegativePageNumber() throws Exception {
+        mockMvc.perform(get("/" + RecruitmentController.RECRUITMENT_URL + "?page=-5")) // allowed to get static fields from classes in main?
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name(containsString(REDIRECT + RecruitmentController.RECRUITMENT_URL)));
     }
 
     @Test
-    public void testRecruitmentPageNonAuthorized() throws Exception {
+    @WithMockUser(authorities = {"recruiter"})
+    public void testRecruitmentPageRedirectAboveMaxPageNumber() throws Exception {
+        mockMvc.perform(get("/" + RecruitmentController.RECRUITMENT_URL + "?page=99999999")) // allowed to get static fields from classes in main?
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name(containsString(REDIRECT + RecruitmentController.RECRUITMENT_URL)));
+    }
+
+    @Test
+    @WithMockUser(authorities = {"applicant"})
+    public void testRecruitmentRedirectPageApplicantNonAuthorized() throws Exception {
+        mockMvc.perform(get("/" + RecruitmentController.RECRUITMENT_URL))
+                .andExpect(status().is4xxClientError())
+                .andExpect(forwardedUrl("/" + LoginController.LOGIN_PAGE_URL));
+    }
+
+    // spring security..
+    @Test
+    public void testRecruitmentRedirectPageNonAuthorized() throws Exception {
         mockMvc.perform(get("/" + RecruitmentController.RECRUITMENT_URL))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(content().string(
-                        containsString("Login")
-                ));
+                .andExpect(redirectedUrl(LOCALHOST + LoginController.LOGIN_PAGE_URL));
     }
+
+
 }
