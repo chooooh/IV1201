6e896b67672afe12e2f7f681d1a1fc3e05d0dba9
malmisen <sebastien.malmberg@hotmail.com>
2022-03-04 12:34:14 +0100

    dummy test

--- src/test/java/se/kth/recruitmentapp/presentation/controller/RecruitmentControllerTest.java
+++ src/test/java/se/kth/recruitmentapp/presentation/controller/RecruitmentControllerTest.java
@@ -1,10 +1,14 @@
 package se.kth.recruitmentapp.presentation.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(RecruitmentController.class)
 class RecruitmentControllerTest {
-
+    @Test
+    void exampleTest(){
+        assertEquals(1,1);
+    }
 }
